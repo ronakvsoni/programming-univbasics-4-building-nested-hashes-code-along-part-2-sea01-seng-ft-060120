567ab1088b1bcb47d13da0206c87916211b0d643
Ronak V Soni <ronakvsoni@gmail.com>
2020-05-26 02:51:55 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -21,8 +21,7 @@ def update_status
    }
   
   epic_tragedy[:montague][:hero][:status]="dead"
-  
-   epic_tragedy[:Capulet][:heroine][:status]="dead"
+   epic_tragedy[:capulet][:heroine][:status]="dead"
    epic_tragedy
  end
  p update_status
